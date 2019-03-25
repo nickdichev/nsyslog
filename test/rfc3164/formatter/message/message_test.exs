2ced869dea49506450a18ff7b4bc25d96fd8490f
Nick Dichev <nick@dichev.email>
2019-03-24 20:59:03 -0700

    added message_test.exs

---
+++ test/rfc3164/formatter/message/message_test.exs
@@ -0,0 +1,8 @@
+defmodule RSyslog.RFC3164.Formatter.Message.Test do
+  use ExUnit.Case, async: true
+  alias RSyslog.RFC3164.Formatter.Message
+
+  test "tags message" do
+    assert Regex.match?(~r/rsyslog\[\d+\.\d+\.\d+\]: test message/, Message.get("test message"))
+  end
+end 
