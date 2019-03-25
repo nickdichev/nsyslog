2f428903d6d636eaf25719269f0e3249628fab5f
Nick Dichev <nick@dichev.email>
2019-03-24 21:45:02 -0700

    added formatter_test.exs

---
+++ test/rfc3164/formatter_test.exs
@@ -0,0 +1,23 @@
+defmodule RSyslog.RFC3164.Formatter.Test do
+  use ExUnit.Case, async: true
+  alias RSyslog.RFC3164.Formatter
+  alias RSyslog.RFC3164.Formatter.Priority
+
+  test "errors on invalid facility" do
+   assert Formatter.format("test message", -1, 3) == {:error, :facility_level}
+  end
+
+  test "errors on invalid severity" do
+   assert Formatter.format("test message", 1, -3) == {:error, :severity_level}
+  end
+
+  test "formats valid message" do
+    {:ok, msg} = Formatter.format("test message")
+    {:ok, pri} = Priority.get(14, 6)
+    {:ok, host} = :inet.gethostname()
+    host = host |> to_string()
+    app = "rsyslog"
+    regex = ~r/#{pri}(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{2} \d{2}:\d{2}:\d{2} #{host} #{app}\[\d+\.\d+\.\d+\]:/
+    assert Regex.match?(regex, msg)
+  end
+end
