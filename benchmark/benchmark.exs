9bf9717dc88ed2297cc5f44506a4a2481751c6bb
Nick Dichev <nick@dichev.email>
2019-03-31 22:09:44 -0700

    benchmark over 3 connections

--- benchmark/benchmark.exs
+++ benchmark/benchmark.exs
@@ -2,13 +2,19 @@ alias RSyslog.Writer
 alias RSyslog.Writer.Supervisor
 
 Supervisor.create_writer(%Writer{aid: "ESS1", host: "localhost", port: 514})
+Supervisor.create_writer(%Writer{aid: "ESS2", host: "localhost", port: 514})
+Supervisor.create_writer(%Writer{aid: "ESS3", host: "localhost", port: 514})
+
+possible_aids = ["ESS1", "ESS2", "ESS3"]
+
+aid_list = 
+  Stream.repeatedly(fn -> Enum.random(possible_aids) end)
+  |> Enum.take(10000)
 
 start_time = DateTime.utc_now()
 
-for _ <- 1..10000 do
-  Task.async(Writer, :send, ["ESS1", "hello!"])
-  |> Task.await()
-end
+Task.async_stream(aid_list, Writer, :send, ["hello!"])
+|> Enum.to_list()
 
 end_time = DateTime.utc_now()
 
