192c24ca970d46b11316c29cd64333056659badc
Nick Dichev <nick@dichev.email>
2019-04-10 21:15:58 -0700

    ran mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Rsyslog.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8.1",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      deps: deps()
     ]
   end
 
