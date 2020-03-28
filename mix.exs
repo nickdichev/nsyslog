33be3eb0e6cdf815028fa3bf177576f60c5bfc96
Nick Dichev <nick@dichev.email>
2020-03-27 19:48:18 -0700

    Make required elixir version less strict in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rsyslog.MixProject do
     [
       app: :nsyslog,
       version: "0.1.0",
-      elixir: "~> 1.8.1",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
