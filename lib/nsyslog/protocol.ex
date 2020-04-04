64e8ec57b5a6c9dca06e1a92d2cce179101af2d3
Nick Dichev <nick@dichev.email>
2020-04-04 14:44:10 -0700

    Cleanup facility and severity typespec in NSyslog.Protocol behaviour

--- lib/nsyslog/protocol.ex
+++ lib/nsyslog/protocol.ex
@@ -1,8 +1,10 @@
 defmodule NSyslog.Protocol do
   @type host :: :ssl.host() | :inet.socket_address() | :inet.hostname()
   @type socket :: :gen_tcp.socket() | :ssl.sslsocket()
+  @type facility :: pos_integer() | atom()
+  @type severity :: pos_integer() | atom()
 
   @callback connect(host(), pos_integer()) :: {:ok, any()} | {:error, any()}
   @callback close(socket()) :: :ok | {:error, any()}
-  @callback send(socket(), iodata(), pos_integer(), pos_integer()) :: :ok | {:error, any()}
+  @callback send(socket(), iodata(), facility(), severity()) :: :ok | {:error, any()}
 end
